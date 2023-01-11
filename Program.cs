5d22e8071c851549625a2096ad99e96b33acd531
Lucas Franco de Mello <luk4w@outlook.com>
2023-01-11 16:41:56 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DotnetChess
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
