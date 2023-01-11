629de06d624cd6ab9fee196c662ac053cecef0dd
Lucas Franco de Mello <luk4w@outlook.com>
2023-01-11 20:47:23 -0300

    created position

---
+++ Engine/Position.cs
@@ -0,0 +1,21 @@
+namespace Engine
+{
+    public class Position
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Position() { }
+
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+    }
+}
