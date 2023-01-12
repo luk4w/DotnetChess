140b3566802d928e4c52ee8d21ce7b07e1836114
Lucas Franco de Mello <luk4w@outlook.com>
2023-01-11 21:35:32 -0300

    printed chess board

--- ConsoleGame.cs
+++ ConsoleGame.cs
@@ -1,4 +1,5 @@
 using Engine;
+using Pieces;
 
 namespace DotnetChess
 {
@@ -21,23 +22,35 @@ namespace DotnetChess
 
         public override void UpdateBoard()
         {
-
+            ToString();
+            Console.ReadLine();
         }
-
 
         public override string ToString()
         {
-            Console.BackgroundColor = ConsoleColor.DarkGray;
-            Console.ForegroundColor = ConsoleColor.DarkBlue;
-
             for (int i = 0; i < 8; i++)
             {
+                Console.BackgroundColor = ConsoleColor.DarkGray;
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                Console.Write($" {8 - i} ");
+                for (int j = 0; j < 8; j++)
+                {
+                    if (i % 2 == 0 && j % 2 != 0 || i % 2 != 0 && j % 2 == 0)
+                        Console.BackgroundColor = ConsoleColor.DarkBlue;
+                    else
+                        Console.BackgroundColor = ConsoleColor.DarkGray;
 
+                    if (Board[i, j] is not Empty)
+                        Console.Write($" {Board[i, j].ToString()} ");
+                    else
+                        Console.Write("   ");
+                }
+                Console.WriteLine();
             }
-            
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("    a  b  c  d  e  f  g  h ");
             return string.Empty;
         }
-
-        
     }
 }
