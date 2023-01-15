f67ec0eedab74c641dd522254c8bd7e5637db5a4
Lucas Franco de Mello <luk4w@outlook.com>
2023-01-15 18:23:51 -0300

    added knight logic

--- Engine/Pieces/Knight.cs
+++ Engine/Pieces/Knight.cs
@@ -10,6 +10,16 @@ namespace Pieces
         public override bool[,] GetMoves(Position from)
         {
             bool [,] moves = new bool[8,8];
+            int[] rowJumps = { -2, -2, -1, -1, 1, 1, 2, 2 };
+            int[] colJump = { -1, 1, -2, 2, -2, 2, -1, 1 };
+
+            for (int i = 0; i < 8; i++)
+            {
+                int x = from.X + rowJumps[i];
+                int y = from.Y + colJump[i];
+                if (x >= 0 && x < 8 && y >= 0 && y < 8 && (Board[x, y] is Empty || Board[x, y].Color != Color))
+                    moves[x, y] = true;             
+            }
             return moves;
         }
 
